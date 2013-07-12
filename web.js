9b18907d612504c3b7bab7511ff7b357ba3757c6
rashmiananthan <rashmi_ananthan88@yahoo.com>
2013-07-12 01:09:55 +0000

    updated web.js with index.html after error1

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.readFileSync('/index.html'));
+var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'));
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
