d633dd1c666f265e9458b52d54942c552dfcc10b
rashmiananthan <rashmi_ananthan88@yahoo.com>
2013-07-12 15:30:14 +0000

    seventh time

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('./index.html',function(err, data){
-  app.get('/', function(request, response){
-      response.send(data.toString());
+app.get('/', function(request, response){
+     var fs = require('fs');
+     var buf = fs.readFileSync("index.html");
+     response.send(buf.toString());
   });
-});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
