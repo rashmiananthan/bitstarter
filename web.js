bebe08db04584481e98f003c538ab8d7cb21e64b
rashmiananthan <rashmi_ananthan88@yahoo.com>
2013-07-12 15:19:49 +0000

    rewriting sixth time

--- web.js
+++ web.js
@@ -2,13 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  var fs = require('fs'); 
-  var buf = fs.readFileSync("index.html");
-  response.send(buf));
-  console.log("writing index.html");
+fs.readFileSync('./index.html',function(err, data){
+  app.get('/', function(request, response){
+      response.send(data.toString());
+  });
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
