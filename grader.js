524402aecc54fc24ba3fa03a6fa14d17770d60ab
rashmiananthan <rashmi_ananthan88@yahoo.com>
2013-07-12 21:24:11 +0000

    rectifying second error

--- grader.js
+++ grader.js
@@ -29,7 +29,7 @@ var checkHtmlFile = function(htmlfile, checksfile){
     var out = {};
     for(var ii in checks){
        var present = $(checks[ii]).length > 0;
-       out[checks[ii] = present;	
+       out[checks[ii]] = present;	
     }
     return out;
 }
